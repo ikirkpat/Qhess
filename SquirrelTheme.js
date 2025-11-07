35a7dc492de02214e21972d63ca303803bbd515c
Ian Kirkpatrick <ikirkpat@oaisd.org>
2025-11-07 12:46:55 -0500

    Squirrels!!!!!!!

--- SquirrelTheme.js
+++ SquirrelTheme.js
@@ -13,6 +13,39 @@ class SquirrelTheme {
             if (piece === '♙') {
                 return 'https://admin-dev.michdev.org/images/squirrel-dancing-squirrel.gif';
             }
+            if (piece === '♟') {
+                return 'https://i.pinimg.com/originals/b2/5e/bf/b25ebf2fa97fc8c06ddb9cd99f999f9a.gif';
+            }
+            if (piece === '♖') {
+                return 'https://media.tenor.com/vfn-ZFnYViUAAAAM/yeah-squirrel.gif';
+            }
+            if (piece === '♜') {
+                return 'https://i.pinimg.com/originals/9d/db/c6/9ddbc6f04145c2e7086c4bb9b2bc6ad5.gif';
+            }
+            if (piece === '♘') {
+                return 'https://media4.giphy.com/media/v1.Y2lkPTZjMDliOTUybW80NXI2ZnQ1bXhweWVpZ3h5b2VkOXVid2NsZXp6Zjdud2F1aHEzZyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/WIBreSdARYNt6/200w.gif';
+            }
+            if (piece === '♞') {
+                return 'https://media4.giphy.com/media/v1.Y2lkPTZjMDliOTUydWE5cTFha3hteGN0ZjBuZHNidGdjZzBodWhkdTZ1cmwzd2l1Ynl6NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/eg4LsfTm6HY0TAsnpg/200w.gif';
+            }
+            if (piece === '♗') {
+                return 'https://dl.glitter-graphics.com/pub/3305/3305949o3u42zrs2f.gif';
+            }
+            if (piece === '♝') {
+                return 'https://i.pinimg.com/originals/b6/e5/2a/b6e52ac37565f955e3133bea91f40c58.gif';
+            }
+            if (piece === '♕') {
+                return 'https://gifdb.com/images/high/graceful-ballet-dancer-dancing-squirrel-b42jytrt8l5wim9f.gif';
+            }
+            if (piece === '♛') {
+                return 'https://giffiles.alphacoders.com/877/87755.gif';
+            }
+            if (piece === '♔') {
+                return 'https://media0.giphy.com/media/3osxY7eI6enqNBo2mQ/giphy.gif';
+            }
+            if (piece === '♚') {
+                return 'https://media.tenor.com/GEEgVauNAm8AAAAM/screaming-yell.gif';
+            }
             // Return null for other pieces to use default Unicode rendering
             return null;
         };
